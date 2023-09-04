c0095d034cc703f763c016971bcc83c4dabaf756
Sparsh_Arya <sparshcodingiiti@gmail.com>
2023-09-04 20:17:35 +0530

    Calling other arrow functions

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import { Fragment } from 'react';
 import './App.css';
 
+const Person = () => {
+  return(
+    <>
+    <h1>Name: Sparsh</h1>
+    <h1>Last Name: Arya</h1>
+    <h1>Age: 24</h1>
+    </>
+  )
+}
 const App = () => {
-  const name = 'John';
-  const isNameShowing = false;
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing?name:"Someone!!!"}</h1>
-    {name ?
-    ( 
-      <>
-      <p>You are there</p>
-      <p>and your name is {isNameShowing?" ":"not "}showing. </p>
-      </>
-    )
-    
-    :
-    
-    ( <>
-      <p>You are not there</p>
-      <p>and your name is {isNameShowing?" ":"not "}showing. </p>
-      </>
-    )}
+      <Person/>
     </div>
   );
 }
