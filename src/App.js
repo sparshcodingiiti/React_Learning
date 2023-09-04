bb882499db4a45b43e7d616b47f787448883ed26
Sparsh_Arya <sparshcodingiiti@gmail.com>
2023-09-04 20:22:57 +0530

    Using Props in React

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { Fragment } from 'react';
 import './App.css';
 
-const Person = () => {
+const Person = (props) => {
   return(
     <>
-    <h1>Name: Sparsh</h1>
-    <h1>Last Name: Arya</h1>
-    <h1>Age: 24</h1>
+    <h1>Name: {props.name}</h1>
+    <h1>Last Name: {props.lastname}</h1>
+    <h1>Age: {props.age}</h1>
     </>
   )
 }
 const App = () => {
   return (
     <div className="App">
-      <Person/>
+      <Person name = "John" lastname = "doe" age = "30"/>
+      <Person name = "Jane" lastname = "stone" age = "19"/>
+      <Person name = "Jenny" lastname = "olson" age = "40"/>
     </div>
   );
 }
