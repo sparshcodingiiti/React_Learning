989b3d3d429de0f940b4e4729b9d5a866472e813
Sparsh_Arya <sparshcodingiiti@gmail.com>
2023-09-04 20:13:27 +0530

    Fragments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { Fragment } from 'react';
 import './App.css';
 
 const App = () => {
   const name = 'John';
-  const isNameShowing = true;
+  const isNameShowing = false;
   return (
     <div className="App">
-    {name ?
-    <p>You are there</p>: <p>You are not there</p>
-    }
       <h1>Hello, {isNameShowing?name:"Someone!!!"}</h1>
+    {name ?
+    ( 
+      <>
+      <p>You are there</p>
+      <p>and your name is {isNameShowing?" ":"not "}showing. </p>
+      </>
+    )
+    
+    :
+    
+    ( <>
+      <p>You are not there</p>
+      <p>and your name is {isNameShowing?" ":"not "}showing. </p>
+      </>
+    )}
     </div>
   );
 }
