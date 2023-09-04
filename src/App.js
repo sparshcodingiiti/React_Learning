2bafde1a188c65b4b4e29529fa231a5fa4f6316e
Sparsh_Arya <sparshcodingiiti@gmail.com>
2023-09-04 20:43:02 +0530

    Using UseState and hooks for events

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import { Fragment } from 'react';
-import './App.css';
-
-const Person = (props) => {
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h1>Last Name: {props.lastname}</h1>
-    <h1>Age: {props.age}</h1>
-    </>
+import React from 'react'
+import { useState } from 'react'
+const App = () => {
+  const [counter,setcounter] = useState(0);
+  return (
+    <div classname="App">
+      <button onClick={()=> setcounter(prevCount=> prevCount -1)}> - </button><br></br>
+      <button>{counter}</button><br></br>
+      <button onClick={()=> setcounter(prevCount=> prevCount +1)}> + </button><br></br>
+    </div>
   )
 }
-const App = () => {
-  return (
-    <div className="App">
-      <Person name = "John" lastname = "doe" age = {30}/>
-      <Person name = "Jane" lastname = "stone" age = {19}/>
-      <Person name = "Jenny" lastname = "olson" age = {40}/>
-    </div>
-  );
-}
 
-export default App;
-
+export default App
